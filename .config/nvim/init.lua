2c6a548ce721ae7aefcb4bf8418c8b1d1975b50f
mattisafur <46404761+mattisafur@users.noreply.github.com>
2026-02-05 22:19:00 +0100

    switched nvim config to lua based script

---
+++ .config/nvim/init.lua
@@ -0,0 +1,13 @@
+vim.opt.number = true
+
+vim.opt.mouse = "a"
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
--- .config/nvim/init.vim
+++
@@ -1,9 +0,0 @@
-set number
-set mouse=a
-set clipboard=unnamedplus
-
-set expandtab
-set tabstop=4
-set shiftwidth=4
-set smartindent
-
